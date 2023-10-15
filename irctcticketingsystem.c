d921500b0964db0ca9c7f78084d89df196f9a144
Aryan Yalavarthi <146920196+mcbanana3@users.noreply.github.com>
2023-10-15 09:49:01 +0530

    Create irctcticketingsystem.c
    You are tasked with developing C program for the Indian Railway Ticket Cancellation System. In this system, passengers can cancel their train tickets and calculate the refund amount based on certain conditions. Your program should include the following features:
    
    The program should ask the user for the following information:
    
    ·        Ticket fare (in INR).
    
    ·        Date of cancellation (in dd/mm/yyyy format).
    
    ·        Date of the scheduled journey (in dd/mm/yyyy format).
    
    The program should then calculate the refund amount based on the following rules:
    
    ·        If the cancellation is requested more than 2 days before the journey, the passenger gets a 75% refund on the ticket fare.
    
    ·        If the cancellation is requested between 1 and 2 days before the journey, the passenger gets a 50% refund on the ticket fare.
    
    ·        If the cancellation is requested on the journey date, no refund is provided.
    
    Date Validation:
    
    ·        Ensure that the entered cancellation date and journey date are valid dates (consider leap years, valid months, and the number of days in each month).
    
    Display Information:
    
    Display the original ticket fare, the calculated refund amount, and the final refund amount to the user.
    
    Your task is to write a C program that implements this ticket cancellation system. Use conditional statements (if-else) to determine the refund amount based on the given conditions. Ensure that the program validates the input dates.
    
    **Input Format:**
    The program expects the following input:
    
     The ticket fare (in INR) as a floating-point number.
     The cancellation date in the format "dd/mm/yyyy," where "dd" is the day, "mm" is the month, and "yyyy" is the year.
     The journey date in the format "dd/mm/yyyy," where "dd" is the day, "mm" is the month, and "yyyy" is the year.
    ***Ensure that each input is provided on a separate line. ***
    
    **Constraints:**
    Ticket fare should be a positive floating-point number.
    The year in the cancellation and journey dates should be in the range from 1900 to 2100.
    The month in the cancellation and journey dates should be in the range from 1 to 12.
    The day in the cancellation date should be valid based on the specified month and year.
    The day in the journey date should be valid based on the specified month and year.
    **Output:**
    
    The program will produce the following output:
    
    The original ticket fare as a floating-point number with two decimal places.
    The calculated refund amount as a floating-point number with two decimal places.
    If the input dates are invalid, the program will output "INVALID-DATE" without any calculation.
    Example:
    
    Input:
    
    3500.50
    02/09/2023
    05/09/2023
    
    Output:
    
    3500.50
    2625.38
    
    In this example, the program takes the ticket fare, cancellation date, and journey date as input and calculates the refund amount based on the specified rules. The original ticket fare is INR 3500.50, and the refund amount is calculated to be INR 2625.38.
    
    INPUT FORMAT
    
    Program should prompt the input from user as follows:
    
    Ticket Fare in Float data type  (First Line)
    
    Cancel Date in the format: DD/MM/YYYY (second Line)
    
    Journey Date in the format: DD/MM/YYYY  (3rd Line)
    
    Output Format:
    
    Output Contains 2 lines. First Line displays Original Ticket Fare and Second line displays Refund Amount
    
     Sample Input -1:
    
    1000.00
    
    10/10/2023
    
    13/10/2023
    
    Output:
    
    1000.00
    
    750.00
    
     Sample Input -2:
    
    2000.00
    
    10/14/2023
    
    13/10/2023
    
    Output:
    
    INVALID-DATE
    
     Sample Input -3:
    
    1400.00
    
    10/10/2023
    
    10/10/2023
    
    Output:
    
    1400.00
    
    0.00

---
+++ irctcticketingsystem.c
@@ -0,0 +1,212 @@
+//IRCTC Railway Ticket refund System
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isleapyear(int year){
+
+    //To Check Leap Year.
+
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool isdatevalid(int cancelation_date, int cancelation_month, int cancelation_year){
+
+    //to Check if Date is Valid or Invalid.
+
+    int normal_year[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (cancelation_year >= 1900 && cancelation_year <= 2100)
+    {
+        if (cancelation_month >= 1 && cancelation_month <= 12)
+        {
+            if (cancelation_month == 2)
+            {
+                // to check if year is leap or not
+                if(isleapyear(cancelation_year))
+                {
+                    if(cancelation_date>=1 && cancelation_date<=29){
+                        return true;
+                    }
+                        
+                    else{
+                        return false;
+                    }
+                }
+                
+                else
+                {
+                    if(cancelation_date>=1 && cancelation_date<=28){
+                        return true;
+                    }
+
+                    else{
+                        return false;
+                    }
+                }
+            }
+
+            else{
+                //check no of days in months
+                int max_days = normal_year[cancelation_month];
+
+                if (cancelation_date >= 1 && cancelation_date <= max_days) 
+                {
+                    return true;
+                }
+
+                else{
+                    return false;
+                }
+            }   
+        }
+
+        else{
+            return false;
+        }
+        
+    }
+
+    else{
+        return false;
+    }
+}
+
+bool zerorefund(int cancelation_date, int reservation_date, int cancelation_month, int reservation_month, int cancelation_year, int reservation_year){
+
+    //zero reund case
+
+    printf("%d/%d/%d & %d/%d/%d\n", cancelation_date, cancelation_month, cancelation_year, reservation_date, reservation_month, reservation_year);
+
+    if((cancelation_year >= reservation_year) && (cancelation_month >= reservation_month) && (cancelation_date >= reservation_date)){
+        printf("Got True\n");
+        return true;
+    }
+
+    else{
+        printf("Got False\n");
+        return false;
+    }
+}
+
+void getpreviousdate(int reservation_date, int reservation_month, int reservation_year, int *prevdd, int *prevmm, int * prevyy){
+    int normal_year[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    //to get previous date from the reservation date
+
+    if(reservation_date >= 2){
+        *prevdd = reservation_date - 1;
+        *prevmm = reservation_month;
+        *prevyy = reservation_year;
+    }
+
+    //to check months and get prevoius dates excluding january and march
+
+    else{
+        if(reservation_month != 3 && reservation_month != 1){
+            *prevdd = normal_year[reservation_month-1];
+            *prevmm = reservation_month - 1;
+            *prevyy = reservation_year;
+        }
+
+        //case for march i.e february leap or not
+
+       else if(reservation_month == 3){
+            if(isleapyear(reservation_year) == true){
+                *prevdd = 29;
+            }
+
+            else{
+                *prevdd = 28;
+            }
+            
+            *prevmm = reservation_month - 1;
+            *prevyy = reservation_year;
+       }
+
+       // case for january as date could be 1st
+
+       else if(reservation_month == 1){
+            *prevdd = 31;
+            *prevmm = 12;
+            *prevyy = reservation_year - 1;
+       }
+    }
+    
+}
+
+bool checkdateequal(int prevdd, int prevmm, int prevyy, int cancelation_date, int cancelation_month, int cancelation_year){
+
+    //To check if the date is equal.
+
+    if(prevdd == cancelation_date && prevmm == cancelation_month && prevyy == cancelation_year){
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+int main(){
+
+    int cancelation_date, cancelation_month, cancelation_year, reservation_date, reservation_month, reservation_year;
+    int prevdd, prevmm, prevyy;
+    float fare;
+
+    // to scan the fare and cancelation dt enad reservation date
+
+    scanf("%f", &fare);
+    scanf("%d/%d/%d", &cancelation_date, &cancelation_month, &cancelation_year);
+    scanf("%d/%d/%d", &reservation_date, &reservation_month, &reservation_year);
+
+    //Check date validity.
+
+    if (isdatevalid(cancelation_date, cancelation_month, cancelation_year) && isdatevalid(reservation_date, reservation_month, reservation_year))
+    {
+
+        //to check if it is zero refund case or not
+
+        if(zerorefund(cancelation_date ,reservation_date,cancelation_month, reservation_month,cancelation_year, reservation_year)){
+            printf("Zero Refund case\n");
+            printf("%.2f\n", fare);
+            printf("0.00\n");
+        }
+
+        // To check if it is 50% refund case or not
+
+        else{
+            getpreviousdate(reservation_date, reservation_month,reservation_year, &prevdd, &prevmm, &prevyy);
+            printf("%d/%d/%d\n", prevdd, prevmm, prevyy);
+            if(checkdateequal(prevdd, prevmm, prevyy, cancelation_date, cancelation_month, cancelation_year)){
+                printf("50 Refund case\n");
+                printf("%.2f\n", fare);
+                printf("%.2f\n", fare/2.0);
+            }
+
+            // to check if it is 75% refund case or not
+
+            else{
+                printf("75 Refund case\n");
+                printf("%.2f\n", fare);
+                printf("%.2f\n", fare * (3.0/4.0));
+            }
+        }
+    }
+
+    // TO print if the date is invalid or not
+
+    else{
+        printf("INVALID-DATE\n");
+    }
+
+    return 0;
+}
